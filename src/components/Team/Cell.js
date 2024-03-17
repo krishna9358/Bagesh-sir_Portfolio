1468e25d6aed1c5222b864f2f087d8e6389bae6b
Krishna Mohan <krishanmohank974@gmail.com>
2024-03-17 15:11:03 +0530

    changes in team data

--- src/components/Team/Cell.js
+++ src/components/Team/Cell.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Cell = ({ data }) => (
-  <div className="cell-container" style={{ margin: '20px' }}>
+  <div className="cell-container" style={{ margin: '50px' }}>
     <article className="mini-post" style={{ width: 'fit-content' }}>
       <header style={
                       {
@@ -22,13 +22,16 @@ const Cell = ({ data }) => (
                   width: '230px',
                   height: '230px',
                   borderRadius: '100%',
-                  border: '10px solid grey',
+                  border: '5px solid grey',
+                  padding: '5px',
                 }
             }
         />
       </header>
       <center><h3 style={{ padding: '0px', margin: '0px' }}>{data.name}</h3></center>
       <center><h5 style={{ padding: '0px', margin: '0px' }}>{data.education}</h5></center>
+      <center><a href={data.linkedin} target='_blank'><img src={data.linkedinImg} style={{width: '20px',height: '20px', marginRight : '10px', marginTop : '20px'}} /></a>
+      <a href={data.github} target='_blank'><img src={data.githubImg} style={{width: '20px',height: '20px'}} /></a></center>
       
     </article>
   </div>
--- src/data/team.js
+++ src/data/team.js
@@ -2,59 +2,132 @@ const data = [
   {
     link: '/images/team/pk.jpeg',
     name: 'Pratiksh Kumar',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
   },
   {
     link: '/images/team/yp.jpeg',
     name: 'Yuvraj Pandey',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
+
   },
   {
     link: '/images/team/ab.jpeg',
     name: 'Akash Bhardwaj',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
   },
   {
     link: '/images/team/sp.jpeg',
     name: 'Shubham Panda',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
+
   },
   {
     link: '/images/team/ps.jpeg',
     name: 'Prakhar Shukla',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
   },
   {
     link: '/images/team/km.jpeg',
     name: 'Krishna Mohan',
-    education : 'B.Tech JECRC University'
+    education : 'Student B.Tech',
+    linkedin : 'https://www.linkedin.com/in/krishna-mohan-194124167/',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+     github : 'https://www.github.com/krishna9358'
   },
   {
-    link: '/images/team/km.jpeg',
+    link: '/images/team/rg.jpeg',
     name: 'Rishik Gupta',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
   },
   {
-    link: '/images/team/km.jpeg',
+    link: '/images/team/au.jpeg',
     name: 'Abhinav Upadhyay',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
   },
   {
-    link: '/images/team/km.jpeg',
+    link: '/images/team/od.jpeg',
     name: 'Om Dabriyal',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
+
   },
   {
-    link: '/images/team/km.jpeg',
+    link: '/images/team/ps1.jpeg',
     name: 'Pranjal Singh',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
+
   },
   {
-    link: '/images/team/km.jpeg',
+    link: '/images/team/ap.jpeg',
     name: 'Aditya Prakash',
+    education : 'Student B.Tech',
+    linkedin : 'https://www.linkedin.com/in/aditya-prakash-a23337254/',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
   },
   {
-    link: '/images/team/km.jpeg',
+    link: '/images/team/a.jpeg',
     name: 'Aryan',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
   },
   {
-    link: '/images/team/km.jpeg',
+    link: '/images/team/as.jpeg',
     name: 'Alankar Singh',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
   },
   {
     link: '/images/team/lm.jpg',
     name: 'Lavish Meena',
+    education : 'Student B.Tech',
+    linkedin : 'linkedin.com',
+    linkedinImg : 'https://imgs.search.brave.com/fQPr1e-Xdvt8sGDU2azrU80WfA8mqPqdzDaBsTf19ck/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS81/MTIvMTc0LzE3NDg1/Ny5wbmc',
+    githubImg : 'https://imgs.search.brave.com/LUckTVtnBmHLsv6OcyA_UsC-qZchQgYysxns3UjMp1s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvaW9uaWNvbnMt/ZmlsbC12b2wtMi81/MTIvbG9nby1naXRo/dWItMTI4LnBuZw',
+
   },
 ];
 
