27e046c4bb60bca79dc23d5bcc4aebecfc94e49a
Krishna Mohan <krishanmohank974@gmail.com>
2024-03-21 00:36:56 +0530

    added events

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -32,6 +32,66 @@ const data = [
   {
     link: '/images/gallery/9.jpeg',
   },
+  {
+    link: '/images/gallery/12.jpg',
+  },
+  {
+    link: '/images/gallery/13.jpg',
+  },
+  {
+    link: '/images/gallery/14.jpg',
+  },
+
+  {
+    link: '/images/gallery/15.jpg',
+  },
+
+  {
+    link: '/images/gallery/16.jpg',
+  },
+  {
+    link: '/images/gallery/17.jpg',
+  },
+  {
+    link: '/images/gallery/18.jpg',
+  },
+  {
+    link: '/images/gallery/19.jpg',
+  },
+  {
+    link: '/images/gallery/20.jpg',
+  },
+  {
+    link: '/images/gallery/21.jpg',
+  },
+  {
+    link: '/images/gallery/22.jpg',
+  },
+  {
+    link: '/images/gallery/23.jpg',
+  },
+  {
+    link: '/images/gallery/24.jpg',
+  },
+  {
+    link: '/images/gallery/25.jpg',
+  },
+  {
+    link: '/images/gallery/26.jpg',
+  },
+  {
+    link: '/images/gallery/27.jpg',
+  },
+  {
+    link: '/images/gallery/28.jpg',
+  },
+  {
+    link: '/images/gallery/29.jpg',
+  },
+  {
+    link: '/images/gallery/30.jpg',
+  },
+
 ];
 
 export default data;
