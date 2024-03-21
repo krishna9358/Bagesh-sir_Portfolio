b23f41eaa0ca52e0e5ed43ab6ff298d67a067cf3
el3v3n0110 <arorapavitra34@gmail.com>
2024-03-21 17:45:11 +0530

    made the layout

--- src/components/Gallery/Cell.js
+++ src/components/Gallery/Cell.js
@@ -5,17 +5,20 @@ const Cell = ({ data }) => (
   <div className="cell-container" style={{ margin: '20px' }}>
     <article className="mini-post" style={{ width: 'fit-content' }}>
       <header style={
-                      {
-                        maxWidth: '400px',
-                        maxHeight: '600px',
-                        overflow: 'hidden',
-                        display: 'flex',
-                        padding: '10px',
-                      }
-                }
+        {
+          maxWidth: '400px',
+          maxHeight: '600px',
+          overflow: 'hidden',
+          display: 'flex',
+          padding: '10px',
+        }
+      }
       >
         <img src={data.link} alt={data.cap} style={{ width: '100%', height: 'auto' }} />
       </header>
+      <div className="caption">
+        {data.caption}
+      </div>
     </article>
   </div>
 );
@@ -24,6 +27,8 @@ Cell.propTypes = {
   data: PropTypes.shape({
     link: PropTypes.string.isRequired,
     cap: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+
   }).isRequired,
 };
 
