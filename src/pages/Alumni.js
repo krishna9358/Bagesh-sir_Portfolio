c17839d8a8a1c0d89ee69802d694b6542e70f226
el3v3n0110 <arorapavitra34@gmail.com>
2024-03-23 02:26:14 +0530

    added the alumni page

---
+++ src/pages/Alumni.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Main from '../layouts/Main';
+
+import Cell from '../components/Team/Cell';
+import data from '../data/team';
+
+const Alumni = () => (
+    <Main
+    title="Team"
+    description="A glance at the team who work with Prof. Dr. Bagesh Kumar"
+  >
+    <article className="post" id="Team">
+      <header>
+        <div className="title">
+          <h2><Link to="/team">Meet the Team</Link></h2>
+        </div>
+      </header>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {data.map((photo) => (
+          <Cell
+            data={photo}
+            key={photo.cap}
+
+          />
+        ))}
+      </div>
+    </article>
+  </Main>
+);
+
+export default Alumni;
